e17f285b5d044c56455f5801a763a66d4390e40b
Dominik Moritz <domoritz@gmail.com>
2012-09-02 03:11:12 +0100

    Finally a tile based implementation that is way faster (but has some bugs)

--- src/heatmap-leaflet-2.js
+++ src/heatmap-leaflet-2.js
@@ -1,59 +1,170 @@
-/* 
- * heatmap.js 0.0.0.1 Leaflet overlay
+/*
+ * heatmap.js 0.2 Leaflet overlay
  *
  * Copyright (c) 2012, Dominik Moritz
  * Dual-licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and the Beerware (http://en.wikipedia.org/wiki/Beerware) license.
+ *
+ * Attribution for snippets: https://gist.github.com/2566567
  */
- 
-L.TileLayer.HeatMap = L.TileLayer.Canvas.extend({
-    addData: function(dataset) {
-        this._data = dataset;
+
+ L.TileLayer.HeatMap = L.TileLayer.Canvas.extend({
+    options: {
+        async: false,
+        debug: false
     },
 
-    drawTile: function(tile, tilePoint, zoom) {
-        var ctx = tile.getContext('2d');
+    initialize: function (options) {
+        L.Util.setOptions(this, options);
 
-        var nwPoint = tilePoint.multiplyBy(this.options.tileSize);
-        var sePoint = nwPoint.add(new L.Point(this.options.tileSize, this.options.tileSize));
-        var nwCoord = this._map.unproject(nwPoint, ctx.zoom, true);
-        var seCoord = this._map.unproject(sePoint, ctx.zoom, true);
-        var bounds = [nwCoord.lng, seCoord.lat, seCoord.lng, nwCoord.lat];
+        this._cache = {
+            max: 0,
+            bounds: {}
+        };
+
+        this.drawTile = function (tile, tilePoint, zoom) {
+            var ctx = {
+                tile: tile,
+                tilePoint: tilePoint,
+                zoom: zoom
+            };
+
+            if (this.options.debug) {
+                this._drawDebugInfo(ctx);
+            }
+            this._draw(ctx);
+        };
+    },
+
+    addData: function(dataset) {
+        this._data = dataset;
+        array = [];
+        dataset.forEach(function(item){
+            array.push(item.value);
+        });
+        this._cache.max = Math.max.apply(Math, array);
+    },
+
+    _createTileProto: function () {
+        var proto = this._tileProto = L.DomUtil.create('div', 'leaflet-tile');
 
         var tileSize = this.options.tileSize;
-        tile.style.width = tileSize+"px";
-        tile.style.height = tileSize+"px";
-        tile.width = parseInt(tile.style.width);
-        tile.height = parseInt(tile.style.height);
+        proto.style.width = tileSize+"px";
+        proto.style.height = tileSize+"px";
+        proto.width = tileSize;
+        proto.height = tileSize;
+    },
 
+    _createTile: function () {
+        var tile = this._tileProto.cloneNode(false);
+        tile.onselectstart = tile.onmousemove = L.Util.falseFn;
+        return tile;
+    },
+
+    _drawDebugInfo: function (ctx) {
+        var canvas = L.DomUtil.create('canvas', 'leaflet-tile-debug');
+        var tileSize = this.options.tileSize;
+        canvas.width = tileSize;
+        canvas.height = tileSize;
+        ctx['tile'].appendChild(canvas);
+        ctx['canvas'] = canvas;
+
+        var max = this.options.tileSize;
+        var g = ctx.canvas.getContext('2d');
+        g.strokeStyle = '#000000';
+        g.fillStyle = '#FFFF00';
+        g.strokeRect(0, 0, max, max);
+        g.font = "12px Arial";
+        g.fillRect(0, 0, 5, 5);
+        g.fillRect(0, max - 5, 5, 5);
+        g.fillRect(max - 5, 0, 5, 5);
+        g.fillRect(max - 5, max - 5, 5, 5);
+        g.fillRect(max / 2 - 5, max / 2 - 5, 10, 10);
+        g.strokeText(ctx.tilePoint.x + ' ' + ctx.tilePoint.y + ' ' + ctx.zoom, max / 2 - 30, max / 2 - 10);
+
+        this._drawPoint(ctx, [0,0])
+    },
+
+    _drawPoint: function (ctx, geom) {
+        var p = this._tilePoint(ctx, geom);
+        var c = ctx.canvas;
+        var g = c.getContext('2d');
+        g.beginPath();
+        g.fillStyle = '#FF0000';
+        g.arc(p.x, p.y, 4, 0, Math.PI * 2);
+        g.closePath();
+        g.fill();
+        g.restore();
+    },
+
+    _tilePoint: function (ctx, coords) {
+        // start coords to tile 'space'
+        var s = ctx.tilePoint.multiplyBy(this.options.tileSize);
+
+        // actual coords to tile 'space'
+        var p = this._map.project(new L.LatLng(coords[1], coords[0]));
+
+        // point to draw
+        var x = Math.round(p.x - s.x);
+        var y = Math.round(p.y - s.y);
+        return {
+            x: x,
+            y: y
+        };
+    },
+
+    _isInTile: function(localXY, padding) {
+        padding = padding || this.options.radius;
+        var bounds = this._cache.bounds[padding];
+        if (!bounds) {
+            var tileSize = this.options.tileSize;
+            var p1 = new L.Point(-padding, -padding), //topLeft
+                p2 = new L.Point(padding+tileSize, padding+tileSize); //bottomRight
+            bounds = this._cache.bounds[padding] = new L.Bounds(p1, p2);
+        };
+        return bounds.contains([localXY.x, localXY.y]);
+    },
+
+    _draw: function(ctx) {
         var options = this.options;
         var config = {
             "radius": options.radius,
-            "element": tile,
+            "element": ctx.tile,
             "visible": true,
             "opacity": options.opacity * 100,
             "gradient": options.gradient
         };
 
-        heatmap = h337.create(config);
+        var heatmap = h337.create(config);
 
-        // todo clip
+        var pointsToDraw = [];
         if (this._data.length > 0) {
             for (var i=0, l=this._data.length; i<l; i++) {
-                var lonlat = new L.LatLng(this._data[i].lat, this._data[i].lon);
-                var localXY = this._map.latLngToLayerPoint(lonlat);
-                localXY = this._map.layerPointToContainerPoint(localXY);
-                heatmap.store.addDataPoint(
-                    Math.floor(localXY.x),
-                    Math.floor(localXY.y),
-                    this._data[i].v);
+
+                var lonlat = [this._data[i].lon, this._data[i].lat];
+                var localXY = this._tilePoint(ctx, lonlat);
+
+                if (this._isInTile(localXY)) {
+                    pointsToDraw.push({
+                        x: localXY.x,
+                        y: localXY.y,
+                        count: this._data[i].value
+                    });
+                };
             }
         }
 
-        ctx.restore();
+        heatmap.store.setDataSet({max: this._cache.max, data: pointsToDraw});
+
+        return this;
+    },
+
+    tileDrawn: function (tile) {
+        this._tileOnLoad.call(tile);
     }
 });
 
 L.TileLayer.heatMap = function (options) {
     return new L.TileLayer.HeatMap(options);
 };
+
