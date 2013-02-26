ec9abc1d96af0b81ef9140b17a06cc0f0b82c9ed
Dominik Moritz <domoritz@gmail.com>
2013-02-26 23:48:07 +0100

    Reimplement leaflet heatmap canvas with fast quadtree index

--- src/heatmap-leaflet.js
+++ src/heatmap-leaflet.js
@@ -5,63 +5,75 @@
  * Dual-licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and the Beerware (http://en.wikipedia.org/wiki/Beerware) license.
  *
- * Attribution for snippets: https://gist.github.com/2566567
+ * Attribution
+ *  - Some snippets for canvas layer: https://gist.github.com/2566567
+ *  - QuadTree: https://github.com/jsmarkus/ExamplesByMesh/tree/master/JavaScript/QuadTree
  */
 
  L.TileLayer.HeatMap = L.TileLayer.Canvas.extend({
-    options: {
-        // calculate the maximum value on a per view basis instead of global
-        // this creates issues when moving the map
-        maxPerView: false,
-        debug: false
-    },
+	options: {
+        debug: false,
+        /*
+         * [EXPERIMENTAL]
+         * true means that the maxValue is calculated per view.
+         * This leads to wrong reporoduction of colors if the map is moved!
+         */
+        useRelativeMaxValue: false
+	},
 
-    initialize: function (options) {
+	initialize: function (options, data) {
+        var self = this;
         L.Util.setOptions(this, options);
-
-        this._cache = {
-            max: 0,
-            bounds: {}
-        };
-
-        this._data = [];
 
         this.drawTile = function (tile, tilePoint, zoom) {
             var ctx = {
-                tile: tile,
+                canvas: tile,
                 tilePoint: tilePoint,
                 zoom: zoom,
                 heatmap: tile.heatmap
             };
 
-            if (this.options.debug) {
-                this._drawDebugInfo(ctx);
+            if (self.options.debug) {
+                self._drawDebugInfo(ctx);
             }
             this._draw(ctx);
         };
     },
 
-    // set a dataset to be drawn
-    // you might want to redraw() if you had a previous dataset
-    setData: function(dataset) {
-        this._data = dataset;
-        this._cache.max = this._calculateMaxValue(dataset);
+    _drawDebugInfo: function (ctx) {
+        var canvas = L.DomUtil.create('canvas', 'leaflet-tile-debug');
+        var tileSize = this.options.tileSize;
+        canvas.width = tileSize;
+        canvas.height = tileSize;
+        ctx.canvas.appendChild(canvas);
+        ctx.dbgcanvas = canvas;
+
+        var max = tileSize;
+        var g = canvas.getContext('2d');
+        g.strokeStyle = '#000000';
+        g.fillStyle = '#FFFF00';
+        g.strokeRect(0, 0, max, max);
+        g.font = "12px Arial";
+        g.fillRect(0, 0, 5, 5);
+        g.fillRect(0, max - 5, 5, 5);
+        g.fillRect(max - 5, 0, 5, 5);
+        g.fillRect(max - 5, max - 5, 5, 5);
+        g.fillRect(max / 2 - 5, max / 2 - 5, 10, 10);
+        g.strokeText(ctx.tilePoint.x + ' ' + ctx.tilePoint.y + ' ' + ctx.zoom, max / 2 - 30, max / 2 - 10);
+
+        this._drawPoint(ctx, [0,0]);
     },
 
-    // add a dataset
-    // you might want to redraw()
-    addData: function(dataset) {
-        // todo: redraw affected tiles
-        this._data.concat(dataset);
-        this._cache.max = Math.max(this._cache.max, this._calculateMaxValue(dataset));
-    },
-
-    // push a single item
-    // you might want to redraw()
-    pushItem: function(lat, lon, value) {
-        // todo: redraw affected tiles
-        this._data.push({'lat': lat, 'lon': lon, 'value': value});
-        this._cache.max = Math.max(value, this._cache.max);
+    _drawPoint: function (ctx, geom) {
+        var p = this._tilePoint(ctx, geom);
+        var c = ctx.dbgcanvas;
+        var g = c.getContext('2d');
+        g.beginPath();
+        g.fillStyle = '#FF0000';
+        g.arc(p.x, p.y, 4, 0, Math.PI * 2);
+        g.closePath();
+        g.fill();
+        g.restore();
     },
 
     _createTileProto: function () {
@@ -92,42 +104,34 @@
         return tile;
     },
 
-    _drawDebugInfo: function (ctx) {
-        var canvas = L.DomUtil.create('canvas', 'leaflet-tile-debug');
-        var tileSize = this.options.tileSize;
-        canvas.width = tileSize;
-        canvas.height = tileSize;
-        ctx['tile'].appendChild(canvas);
-        ctx['canvas'] = canvas;
+    /**
+     * Inserts data into quadtree and redraws heatmap canvas
+     */
+    setData: function(dataset) {
+        var self = this;
+        var latLngs = [];
+        this._maxValue = 0;
+        dataset.forEach(function(d) {
+            latLngs.push(new L.LatLng(d.lat, d.lon));
+            self._maxValue = Math.max(self._maxValue, d.value);
+        });
+        this._bounds = new L.LatLngBounds(latLngs);
 
-        var max = tileSize;
-        var g = ctx.canvas.getContext('2d');
-        g.strokeStyle = '#000000';
-        g.fillStyle = '#FFFF00';
-        g.strokeRect(0, 0, max, max);
-        g.font = "12px Arial";
-        g.fillRect(0, 0, 5, 5);
-        g.fillRect(0, max - 5, 5, 5);
-        g.fillRect(max - 5, 0, 5, 5);
-        g.fillRect(max - 5, max - 5, 5, 5);
-        g.fillRect(max / 2 - 5, max / 2 - 5, 10, 10);
-        g.strokeText(ctx.tilePoint.x + ' ' + ctx.tilePoint.y + ' ' + ctx.zoom, max / 2 - 30, max / 2 - 10);
+        this._quad = new QuadTree(this._boundsToQuery(this._bounds), false, 6, 6);
 
-        this._drawPoint(ctx, [0,0]);
+        dataset.forEach(function(d) {
+            self._quad.insert({
+                x: d.lon,
+                y: d.lat,
+                value: d.value
+            });
+        });
+        this.redraw();
     },
 
-    _drawPoint: function (ctx, geom) {
-        var p = this._tilePoint(ctx, geom);
-        var c = ctx.canvas;
-        var g = c.getContext('2d');
-        g.beginPath();
-        g.fillStyle = '#FF0000';
-        g.arc(p.x, p.y, 4, 0, Math.PI * 2);
-        g.closePath();
-        g.fill();
-        g.restore();
-    },
-
+    /**
+     * Transforms coordinates to tile space
+     */
     _tilePoint: function (ctx, coords) {
         // start coords to tile 'space'
         var s = ctx.tilePoint.multiplyBy(this.options.tileSize);
@@ -138,73 +142,64 @@
         // point to draw
         var x = Math.round(p.x - s.x);
         var y = Math.round(p.y - s.y);
+        return [x, y];
+    },
+
+    /**
+     * Creates a query for the quadtree from bounds
+     */
+    _boundsToQuery: function(bounds) {
         return {
-            x: x,
-            y: y
+            x: bounds.getSouthWest().lng,
+            y: bounds.getSouthWest().lat,
+            width: bounds.getNorthEast().lng-bounds.getSouthWest().lng,
+            height: bounds.getNorthEast().lat-bounds.getSouthWest().lat
         };
     },
 
-    // checks whether the point is inside a tile with padding
-    _isInTile: function(localXY, padding) {
-        padding = (padding || this.options.radius);
-        var bounds = this._cache.bounds[padding];
-        if (!bounds) {
-            var tileSize = this.options.tileSize;
-            var p1 = new L.Point(-padding, -padding); //topLeft
-            var p2 = new L.Point(padding+tileSize, padding+tileSize); //bottomRight
-            bounds = this._cache.bounds[padding] = new L.Bounds(p1, p2);
-        }
-        return bounds.contains([localXY.x, localXY.y]);
-    },
-
-    // get the max value of the dataset
     _getMaxValue: function() {
-        if (this.options.maxPerView) {
-            var dataset = [];
-            var mapBounds = this._map.getBounds();
-            this._data.forEach(function(item){
-                if (mapBounds.contains([item.lat, item.lon])) {
-                    dataset.push(item);
-                }
+        if (this.options.useRelativeMaxValue) {
+            var bounds = this._map.getBounds();
+            var maxValue = 0;
+            this._quad.retrieveInBounds(this._boundsToQuery(bounds)).forEach(function(obj) {
+                maxValue = Math.max(maxValue, obj.value);
             });
-
-            return this._calculateMaxValue(dataset);
+            return maxValue;
         } else {
-            return this._cache.max;
+            return this._maxValue;
         }
     },
 
-    _calculateMaxValue: function(dataset) {
-        array = [];
-        dataset.forEach(function(item){
-            array.push(item.value || item.count);
-        });
-        return Math.max.apply(Math, array);
-    },
+    _draw: function (ctx) {
+        if (!this._quad || !this._map) {
+            return;
+        }
 
-    _draw: function(ctx) {
-
-        var heatmap = ctx.heatmap;
-        heatmap.clear();
-
+        var self = this;
+        var localXY, value;
         var pointsInTile = [];
-        if (this._data.length > 0) {
-            for (var i=0, l=this._data.length; i<l; i++) {
 
-                var lonlat = [this._data[i].lon, this._data[i].lat];
-                var localXY = this._tilePoint(ctx, lonlat);
+        var tileSize = this.options.tileSize;
+        var nwPoint = ctx.tilePoint.multiplyBy(tileSize);
+        var sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
 
-                if (this._isInTile(localXY)) {
-                    pointsInTile.push({
-                        x: localXY.x,
-                        y: localXY.y,
-                        count: this._data[i].value
-                    });
-                }
-            }
-        }
+        // padding
+        var pad = new L.Point(this._getMaxValue(), this._getMaxValue());
+        nwPoint = nwPoint.subtract(pad);
+        sePoint = sePoint.add(pad);
 
-        heatmap.store.setDataSet({max: this._getMaxValue(), data: pointsInTile});
+        var bounds = new L.LatLngBounds(this._map.unproject(sePoint), this._map.unproject(nwPoint));
+        this._quad.retrieveInBounds(this._boundsToQuery(bounds)).forEach(function(obj) {
+            localXY = self._tilePoint(ctx, [obj.x, obj.y]);
+            value = obj.value;
+            pointsInTile.push({
+                x: localXY[0],
+                y: localXY[1],
+                count: value
+            });
+        });
+
+        ctx.heatmap.store.setDataSet({max: this._getMaxValue(), data: pointsInTile});
 
         return this;
     }
