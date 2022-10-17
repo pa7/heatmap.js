b07b217a30265af1b6cb157c6a639078f3a8411f
fumiya-sato-p <fumiya.sato.p@aifield.jp>
2022-10-17 13:44:29 +0900

    fix  pa7#219

--- build/heatmap.js
+++ build/heatmap.js
@@ -4,7 +4,7 @@
  * Copyright 2008-2016 Patrick Wied <heatmapjs@patrick-wied.at> - All rights reserved.
  * Dual licensed under MIT and Beerware license 
  *
- * :: 2016-09-05 01:16
+ * :: 2022-10-17 13:44
  */
 ;(function (name, context, factory) {
 
@@ -524,7 +524,7 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
 
       }
 
-      img.data = imgData;
+      // img.data = imgData;
       this.ctx.putImageData(img, x, y);
 
       this._renderBoundaries = [1000, 1000, 0, 0];
